89db8ef6279939a23777d6e5fd4dcf71d99708cd
Rustypredator <contact@rusty.info>
2024-04-09 15:35:41 +0200

    add config for overviewer (currently unused)

---
+++ config/overviewer/main.py
@@ -0,0 +1,29 @@
+worlds["survival"] = "/srv/cst/survival/world"
+
+worlds["creative"] = "/srv/cst/creative/world"
+
+worlds["testing"] = "/srv/cst/testing/world"
+
+renders["survival_ow_day"] = {
+    "world": "survival",
+    "title": "Survival OW Daytime",
+    "rendermode": smooth_lighting,
+    "dimension": "overworld"
+}
+
+renders["survival_nether"] = {
+    "world": "survival",
+    "title": "Survival Nether",
+    "rendermode": nether_smooth_lighting,
+    "dimension": "nether"
+}
+
+renders["survival_end"] = {
+    "world": "survival",
+    "title": "Survival The End",
+    "rendermode": smooth_lighting,
+    "dimension": "end"
+}
+
+outputdir = "/srv/cst/mapping/output/overviewer"
+texturepath = "/srv/cst/mapping/textures/default"
